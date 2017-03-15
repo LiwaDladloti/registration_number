f9c26d80d9b8c5e696acb7cb01a0855d148e3e2e
LiwaDladloti <liwadladloti@gmail.com>
2017-03-15 13:38:20 +0200

    made a filter to be able to select a specific area and also changed background of the webpage

--- registration_numbers.js
+++ registration_numbers.js
@@ -1,15 +1,41 @@
 function addBtn() {
     'use strict';
-    //declare variebles
-    var ul = document.getElementById("plateNum");
-    //var createdLi, text, text1;
+    var ul = document.getElementById('plateNum');
     var text = document.getElementById('textBox').value;
-    if (text.length > 0 && text !== null){
+    if (text.length > 0 && text !== null) {
         
-    var createdLi = document.createElement('li');
-    createdLi.textContent = text;
-   // text1 = document.createTextNode(text);
-    ul.appendChild(createdLi);
-    }  //document.getElementById('plateNum').appendChild(createdLi);
+        var createdLi = document.createElement('li');
+        createdLi.textContent = text;
+        ul.appendChild(createdLi);
+    }
     document.getElementById('textBox').value = '';
+}
+
+function regNo() {
+    'use strict';
+    var regArea = document.getElementById('optionList').value;
+    var allAreas = document.getElementById('allAreas').innerHTML;
+    var bel = document.getElementById('Bellville').innerHTML;
+    console.log(regArea + "," + bel);
+    var cpt = document.getElementById('capeTown').innerHTML;
+    var paarl = document.getElementById('Paarl').innerHTML;
+    var list = document.getElementsByTagName('li');
+    
+    for (var i = 0; i < list.length; i++) {
+        var listLoop = list[i];
+        var currentReg = list[i].textContent.toUpperCase();
+        console.log(listLoop);
+        if (regArea === allAreas) {
+            listLoop.style.display = 'inline-block';
+        } else if (regArea === bel && currentReg.startsWith('CY')) {
+            console.log("entered CY block")
+            listLoop.style.display = 'inline-block';
+        } else if (regArea === cpt && currentReg.startsWith('CA')) {
+            listLoop.style.display = 'inline-block';
+        } else if (regArea === paarl && currentReg.startsWith('CL')) {
+            listLoop.style.display = 'inline-block';
+        } else {
+            listLoop.style.display = 'none'
+        }
+    }
 }
